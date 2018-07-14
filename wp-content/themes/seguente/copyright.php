e9e08de8cf9284f712ef6995e38f0ded9ba8ac65
Jay Paul Aying <jaypaulaying5@gmail.com>
2018-07-14 13:03:01 +0800

    initial files

---
+++ wp-content/themes/seguente/copyright.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Seguente
+ */  
+$seguente_theme_options = seguente_get_options( 'seguente_theme_options' );
+?>
+<div id="copyright">
+	<div class="copyright-wrap">
+		<span class="right" style="text-align:center;padding-right: 20px;">Copyright 2017. All rights reserved.
+	<br/><br/>In collaboration with Bagol Labs Software</span>
+	</div>
+</div><!--copyright-->
